e9c4801c76f41db5c155383cc84670a44996bd6a
Pedro Gaudencio <pmgaudencio@gmail.com>
2016-12-27 13:21:13 +0700

    led-button: control LED by pressing a button

---
+++ led-button/led_party.py
@@ -0,0 +1,40 @@
+from nanpy import (
+    SerialManager,
+    ArduinoApi
+)
+from time import sleep
+
+
+ledPin = 7
+buttonPin = 8
+ledState = False
+buttonState = 0
+
+
+try:
+    connection = SerialManager()
+    a = ArduinoApi(connection=connection)
+except Exception as e:
+    print("Failed to connect to Arduino. {}".format(e.message))
+    ledState = False
+
+a.pinMode(ledPin, a.OUTPUT)
+a.pinMode(buttonPin, a.INPUT)
+
+try:
+    while True:
+        buttonState = a.digitalRead(buttonPin)
+        print("Our button state is: {}".format(buttonState))
+        if buttonState:
+            if ledState:
+                a.digitalWrite(ledPin, a.LOW)
+                ledState = False
+                print("LED OFF")
+                sleep(1)
+            else:
+                a.digitalWrite(ledPin, a.HIGH)
+                ledState = True
+                print("LED ON")
+                sleep(1)
+except:
+    a.digitalWrite(ledPin, a.LOW)
