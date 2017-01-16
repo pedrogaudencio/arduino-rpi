830c44303b70cb51f2137dc98f97b353541527ca
Pedro Gaudencio <pmgaudencio@gmail.com>
2017-01-17 01:12:57 +0700

    ir-bounce: infrared receiver/transmitter

---
+++ ir-bounce/ir_bouncer.py
@@ -0,0 +1,26 @@
+from time import sleep
+from arduino_gateway import Portal
+
+
+def read_ir():
+    ir_value = gate.digitalRead(ir_receiver)
+    print("IR value: {}".format(ir_value))
+    sleep(1)
+    # if ir_value:
+    #         gate.digitalWrite(ir_transmitter, gate.LOW)
+    #         print("Sent value: {}".format(ir_value))
+    #         sleep(2)
+
+
+if __name__ == "__main__":
+    portal = Portal()
+    portal.open()
+    gate = portal.gate
+
+    ir_receiver = 11
+    ir_transmitter = 11
+
+    gate.pinMode(ir_receiver, gate.INPUT)
+    gate.pinMode(ir_transmitter, gate.OUTPUT)
+
+    portal.loop(read_ir)
